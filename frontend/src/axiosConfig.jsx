7ff84d2b846f5446515eb1936d9adaaae44f8f7c
Xiaoyu <xiaoyuyang48@gmail.com>
2025-08-06 15:08:41 +1000

    baseURL again

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.236.178.32:5001', // live
+  baseURL: 'http://172.31.99.250:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
